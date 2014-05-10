90003d24167aa26ca117b22696a8570b7c262c3f
Yuri Khan <yurivkhan@gmail.com>
2014-05-11 00:14:36 +0700

    SDCC02. Reboot to ISP DFU mode on startup

--- test.c
+++ test.c
@@ -1,4 +1,18 @@
-int main()
+__sfr __at(0xE7) ISPCR;
+#define ISPEN 0x80
+#define SWBS  0x40
+#define SWRST 0x20
+#define CFAIL 0x10
+#define MISPF 0x08
+#define MS1   0x02
+#define MS0   0x01
+
+void _Noreturn reboot_to_isp(void)
 {
-	return 0;
+	ISPCR = SWBS | SWRST | MISPF;
+}
+
+int main(void)
+{
+	reboot_to_isp();
 }
