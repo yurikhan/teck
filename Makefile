143b7a81209397147fd8a1cbc6e34585014ddd9f
Yuri Khan <yurivkhan@gmail.com>
2014-05-25 15:16:45 +0700

    SDCC04. Blinking LED

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ CFLAGS = --xram-size 576 --no-xinit-opt
 all: test.ihx test.listing.orig
 
 %.listing.orig: %.ihx
-	dis51 -l <$< >$@
+	dis51 -l 0 0x0B <$< >$@
 
 %.ihx: %.c Makefile
 	$(CC) $(CFLAGS) $<
--- test.c
+++ test.c
@@ -1,4 +1,5 @@
 #include <stdbool.h>
+#include <8051.h>
 
 __sfr __at(0xE7) ISPCR;
 #define ISPEN 0x80
@@ -23,15 +24,45 @@ __sbit __at(0xB5) nLED3;
 __sbit __at(0xB6) nLED1;
 __sbit __at(0xB7) nLED2;
 
+volatile unsigned char delay_count;
+
+void timer_isr(void) __interrupt(1) __using(1)
+{
+	TR0 = false;
+	if (delay_count)
+	{
+		--delay_count;
+		TL0 = 0x18;
+		TH0 = 0xFC;
+		TR0 = true;
+	}
+}
+
+void delay(unsigned char ms)
+{
+	TR0 = false;
+	delay_count = ms;
+	TL0 = 0x18;
+	TH0 = 0xFC;
+	TR0 = true;
+	while (delay_count) {}
+}
+
 int main(void)
 {
 	if (nDIP4)
 	{
 		reboot_to_isp();
 	}
-	else
+	TMOD = T0_M0;
+	PT0 = true;
+	ET0 = true;
+	EA = true;
+	while (true)
 	{
+		nLED2 = true;
+		delay(250);
 		nLED2 = false;
-		while (true);
+		delay(250);
 	}
 }
