6eadaf8a2d11edc63a0f6080ada12bbb633a2132
Yuri Khan <yurivkhan@gmail.com>
2014-06-18 15:56:32 +0700

    Default state: Receiving requests
    When the host sends us anything, we will receive a USB interrupt with
    the flag `URXD0`. I’m assuming that the MG84FL54’s FIFO is able to
    buffer up to 64 bytes of data (which can accommodate the USB request
    setup token just fine) and give us a USB interrupt with the `RXSETUP`
    flag set in the Endpoint 0’s `RXSTAT` register.
    
    Now we need to read it out of the FIFO. To read the received data, we
    clear `EDOVW` in `RXSTAT` and then repeatedly read the `RXDAT` register.
    
    We then acknowledge the receipt of the Setup token, handle the request,
    and tell the USB subsystem that we’re done with this transfer. If
    request handling fails, we stall the endpoint 0.

--- usb.c
+++ usb.c
@@ -34,6 +34,15 @@ void usb_reset(void) __using(3)
 	set_state(state_default);
 }
 
+void usb_receive(uint8_t __near * buffer, uint8_t size) __using(3)
+{
+	if (size < RXCNT) size = RXCNT;
+	while (size --> 0)
+	{
+		*buffer++ = RXDAT;
+	}
+}
+
 void usb_init(void)
 {
 	set_state(state_powered);
@@ -54,11 +63,33 @@ void usb_init(void)
 
 void usb_isr(void) __interrupt(15) __using(3)
 {
-	unsigned char upcon = UPCON;
+	unsigned char upcon, uiflg;
+
+	upcon = UPCON;
 	if (upcon & URST)
 	{
 		UPCON = upcon & ~(URST | URSM | USUS) | URST;
 		usb_reset();
+		return;
+	}
+
+	uiflg = UIFLG;
+	if (uiflg & URXD0) // Received some bytes
+	{
+		UIFLG = URXD0; // Acknowledge them
+		EPINDEX = 0;
+		if (RXSTAT & RXSETUP) // Got a complete Setup token
+		{
+			EPCON &= ~(TXSTL | RXSTL); // Recover from previous error if any
+			RXSTAT &= ~EDOVW;
+			usb_receive((unsigned char __near *)&request, sizeof(request));
+			RXSTAT &= ~RXSETUP;
+			RXCON |= RXFFRC;
+			if (!usb_request())
+			{
+				EPCON |= TXSTL | RXSTL; // Signal error
+			}
+		}
 		return;
 	}
 }
